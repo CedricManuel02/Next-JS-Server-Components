382edc461ecbb465f27771e6ff81858b62dd950e
Cedric Gio Manuel <113290298+CedricManuel02@users.noreply.github.com>
2024-05-20 08:35:57 +0800

    new push

--- src/app/.next/static/B3Dn1-TRvqZM7kR4-OXQX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-fdc5cf1f34cb592f.js"],"/page":["static/chunks/pages/page-f9f3f74b26f52e2a.js"],sortedPages:["/_app","/_error","/page"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/app/api/insertData.ts
+++ src/app/api/insertData.ts
@@ -1,9 +1,9 @@
+"use server";
 import { PrismaClient } from '@prisma/client'
 import { redirect } from 'next/navigation'
 const prisma = new PrismaClient()
 
 export const insertData = async (formData : FormData) => {
-    "use server";
     const name = formData.get('name')
     const price = formData.get('price')
   
