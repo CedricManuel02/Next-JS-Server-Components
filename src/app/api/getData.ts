729128c41b060dff96e46dd81b5ba626c8166a0f
Cedric Gio Manuel <113290298+CedricManuel02@users.noreply.github.com>
2024-05-20 09:21:55 +0800

    adding revalidatePath

--- src/app/api/getData.ts
+++ src/app/api/getData.ts
@@ -4,7 +4,11 @@ import { Product } from '../interface/types';
 const prisma = new PrismaClient()
 
 export const getData = async () => {
+  try{
     const product : Product[] = await prisma.product.findMany()
     return product
+  }catch(err){
+    return err
+  }
 }
   
--- src/app/api/insertData.ts
+++ src/app/api/insertData.ts
@@ -1,15 +1,12 @@
 "use server";
 import { PrismaClient } from '@prisma/client'
-import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache';
 const prisma = new PrismaClient()
 
 export const insertData = async (formData : FormData) => {
     const name = formData.get('name')
     const price = formData.get('price')
-  
-    const insert = await prisma.product.create({data: {name: name as string, price: parseInt(price as string)}})
-  
-    console.log(insert)
-    redirect("/")
+    await prisma.product.create({data: {name: name as string, price: parseInt(price as string)}})
+    revalidatePath("/")
   }
   
